a97805f7f585082cece3094cc7beb62f8075a210
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-06-04 16:07:42 +0545

    feat: env set up

--- server.js
+++ server.js
@@ -3,17 +3,15 @@ import express from "express";
 //rest object
 const app = express();
 
-//port
-// const PORT = process.env.PORT || 8080;
-//listen
-// app.listen(PORT, () => {
-//   console.log(
-//     `Node Server Running In ${process.env.DEV_MODE} Mode on port no ${PORT}`
-//       .bgCyan.white
-//   );
-// });
-
 app.get("/", (req, res) => {
   res.send("hello");
 });
-app.listen(8080);
+
+//port
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} Mode on port no ${PORT}`
+  );
+});
